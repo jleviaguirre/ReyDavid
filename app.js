e9c237145658b426e62ec5fc9d53d9382ae36ebf
jleviaguirre <github@kamarble.com>
2026-02-28 18:07:02 -0600

    Update app.js

--- app.js
+++ app.js
@@ -212,7 +212,7 @@ function renderTiles(user) {
                 tileEl.onmouseover = () => tileEl.style.transform = "translateY(-4px)";
                 tileEl.onmouseout = () => tileEl.style.transform = "translateY(0)";
                 
-                tileEl.onclick = () => window.openDynamicPage(tab.title);
+                tileEl.onclick = () => { window.location.hash = normalizedTabName; };
                 container.appendChild(tileEl);
             }
         });
